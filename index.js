5faaada23b13a5e7870ce15ab8bcb50bc44b3b37
Linus Hagemann <mail@linushagemann.de>
2024-02-14 16:26:11 +0100

    Test cloning Commited from within lively.next.

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ import { part } from 'lively.morphic/components/core.js';
 import { depComponent } from 'nextguys--private-dependency-project/ui/components.cp.js';
 
 export async function main () {
+  console.log('hello world');
   part(depComponent).openInWorld();
 }
